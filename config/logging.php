17cafb475fcce76fe5b8bd26ac3432c127123100
Alpha Olomi <alphaolomi@gmail.com>
2023-05-17 23:17:23 +0300

    add railways support

--- config/logging.php
+++ config/logging.php
@@ -54,7 +54,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'stdout'],
             'ignore_exceptions' => false,
         ],
 
