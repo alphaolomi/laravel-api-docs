7d1174fd641ba374115f82712e8d57a0d86f78b0
Alpha Olomi <alphaolomi@gmail.com>
2023-05-19 00:20:33 +0300

    add rector

---
+++ rector.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelLevelSetList;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ]);
+
+    $rectorConfig->sets([
+        LaravelLevelSetList::UP_TO_LARAVEL_100,
+        LevelSetList::UP_TO_PHP_81,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+    ]);
+
+    $rectorConfig->rules([
+        // ...
+    ]);
+
+    // https://getrector.com/documentation/ignoring-rules-or-paths
+    $rectorConfig->skip([
+        // ...
+    ]);
+};
