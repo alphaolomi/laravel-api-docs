69debdf03f9ae57f7eebf1fb8612dd054c21e090
Alpha Olomi <alphaolomi@gmail.com>
2023-05-19 00:39:10 +0300

    rector paths

--- rector.php
+++ rector.php
@@ -9,18 +9,10 @@ use RectorLaravel\Set\LaravelLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__.'/app',
-        __DIR__.'/database',
-        __DIR__.'/routes',
-        __DIR__.'/tests',
-
-        // __DIR__ . '/app',
-        // __DIR__ . '/bootstrap',
-        // __DIR__ . '/config',
-        // __DIR__ . '/public',
-        // __DIR__ . '/resources',
-        // __DIR__ . '/routes',
-        // __DIR__ . '/tests',
+        __DIR__ . '/app',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
     ]);
 
     $rectorConfig->sets([
