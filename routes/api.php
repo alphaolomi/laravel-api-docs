f2000363c46717dcf0b516ba59572d173b81d7f1
Alpha Olomi <alphaolomi@gmail.com>
2023-05-19 05:42:57 +0300

    🐛(typo) invoke to class string

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,11 @@ Route::post('/login', (new App\Http\Controllers\AuthController())->login(...));
 Route::post('/register', (new App\Http\Controllers\AuthController())->register(...));
 Route::apiResource('products', App\Http\Controllers\ProductController::class);
 Route::middleware('auth:sanctum')->group(function (): void {
-    Route::get('/user', App\Http\Controllers\ProfileController::__invoke(...));
-    Route::get('/health-check', App\Http\Controllers\HealthController::__invoke(...));
+    Route::get('/user', App\Http\Controllers\ProfileController::class);
+    Route::get('/health-check', App\Http\Controllers\HealthController::class);
     Route::post('/logout', (new App\Http\Controllers\AuthController())->logout(...));
 
     // Route::apiResource('products', App\Http\Controllers\ProductController::class);
 
-    Route::get('/collection', App\Http\Controllers\CollectionController::__invoke(...));
+    Route::get('/collection', App\Http\Controllers\CollectionController::class);
 });
