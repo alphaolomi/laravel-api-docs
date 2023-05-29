aed43ee1fb7a453006b051e09035f9ebff50684a
Alpha Olomi <alphaolomi@gmail.com>
2023-05-29 07:43:51 +0300

    feat: add octane and treblle

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
-Route::apiResource('products', App\Http\Controllers\ProductController::class);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', [App\Http\Controllers\ProfileController::class, '__invoke']);
-    Route::get('/health-check', [App\Http\Controllers\HealthController::class, '__invoke']);
-    Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+Route::middleware(['treblle'])->group(function () {
 
-    // Route::apiResource('products', App\Http\Controllers\ProductController::class);
+    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::apiResource('products', App\Http\Controllers\ProductController::class);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/user', [App\Http\Controllers\ProfileController::class, '__invoke']);
+        Route::get('/health-check', [App\Http\Controllers\HealthController::class, '__invoke']);
+        Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
 
-    Route::get('/collection', [App\Http\Controllers\CollectionController::class, '__invoke']);
+        // Route::apiResource('products', App\Http\Controllers\ProductController::class);
+
+        Route::get('/collection', [App\Http\Controllers\CollectionController::class, '__invoke']);
+    });
+
 });
