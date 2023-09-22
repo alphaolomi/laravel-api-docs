208706efb3385d9215480271b5b21f87e59680f5
Alpha Olomi <alphaolomi@gmail.com>
2023-09-22 10:02:13 +0300

    feat: products index and show are public

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HealthController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([])->group(function () {
-    Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-    Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 
+    Route::apiResource('products', App\Http\Controllers\ProductController::class)->only(['index', 'show']);
 
     Route::middleware('auth:sanctum')->group(function () {
-        Route::get('/user', [App\Http\Controllers\ProfileController::class, '__invoke']);
-        Route::get('/health-check', [App\Http\Controllers\HealthController::class, '__invoke']);
-        Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+        Route::get('/user', [ProfileController::class, '__invoke']);
+        Route::get('/health-check', [HealthController::class, '__invoke']);
+        Route::post('/logout', [AuthController::class, 'logout']);
 
-        Route::apiResource('products', App\Http\Controllers\ProductController::class);
+        Route::apiResource('products', ProductController::class)->only(['store', 'update', 'destroy']);
     });
 });
